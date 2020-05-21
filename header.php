fee6decf29ba30ae27ed739795340be379b59783
Ehabuddin Mohammed <mdehab@live.com>
2020-05-21 12:11:01 -0400

    changes to header

---
+++ header.php
@@ -0,0 +1,44 @@
+<!--Header -->
+<div style="padding-top:10px;" class= "container">
+    <div class="testheader">
+        <nav class="navbar">
+            <div class="container-fluid">
+                <div class="navbar-header col_one">
+                <div style="padding-top:10px;" ><a class="logo" href="#">Viseo Digital</a></div>
+                </div>
+                <ul style="float:right;" class="nav navbar-nav">
+                <li class="active_page nav_items"><a href="#">Home</a></li>
+                <li class="nav_items"><a href="#">About Us</a></li>
+                <li class="nav_items"><a href="#">Services</a></li>
+                <li class="nav_items"><a href="#">Projects</a></li>
+                <li class="nav_items"><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+        </nav>
+        
+        <div style="padding-top: 10%;" class="col_one">
+            <p id="headerimgs" class="ingenious_text">INGENIOUS DESIGN & DEVELOPMENT</p>
+            <img id="headerimgs" style="margin-left: 13%; padding-top:10%;" class="aboutusbutton" src="./button-big.png">
+        </div>
+
+        <div style="padding-top:10%;" class="col_two">
+                <img class="laptopImg" src="./img-des-code.png">
+        </div>
+        
+
+
+
+        <div class="whitespace col_one"></div>
+
+        <div class="col_one" id="AboutUs">
+            <img class="about_us_img" src="./image-5.png">
+        </div>
+
+        <!-- <div class="col_one" style="position:relative;height: 200px;"></div> -->
+        
+        
+        
+        
+    </div>
+
+    <!--/ Header -->
