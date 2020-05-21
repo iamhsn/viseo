ad636cdaa0eac0b40f506b34baa4a17de8b7820e
Ehabuddin Mohammed <mdehab@live.com>
2020-05-21 10:40:11 -0400

    first commit

--- content.php
+++
@@ -1,6 +0,0 @@
-    <div class="whitespace col_one"></div>
-
-    <div class="col_one row" id="AboutUs">
-        <!-- <img class="about_us_img" src="./image-5.png"> -->
-    </div>
-
---
+++ index_v1.php
@@ -0,0 +1,109 @@
+<html>
+    <head>
+        <title>Viseo Digital</title>
+        
+        <meta name="viewport" content="width=device-width height=device-height, initial-scale=1.0">     
+        <!--Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+        <!-- Google Fonts-->
+        <!-- <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@600&display=swap" rel="stylesheet"> -->
+        
+        <!-- <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@500;700&display=swap" rel="stylesheet"> -->
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+        <!--PURE.CSS-->
+        <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/base-min.css">
+        <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
+        <!--PURE GRID-->
+        <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/grids-responsive-min.css" />
+
+        <!--Style Sheet (Figma)-->
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <div class="container">
+            <!-- Header -->
+            <!-- <div id="test"> -->
+            <div class="header pure-u-1-1">
+                <div class="nav pure-menu-horizontal">
+                    <!-- <nav class="nav"> -->                    
+                        <div class="navbar-header">
+                            <button class="navbar-toggler" style="margin-top:10%;" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15" aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button> 
+                            <div class="nav_padding"><a class="logo" href="#">Viseo Digital</a></div>
+                        </div>
+
+                        
+
+                        <ul id="navbarSupportedContent15" style="float:right" class="collapse navbar-collapse navbar-header nav navbar-nav">
+                        <li class="active_page nav_items"><a href="#">Home</a></li>
+                        <li class="nav_items"><a href="#AboutUs">About Us</a></li>
+                        <li class="nav_items"><a href="#services">Services</a></li>
+                        <li class="nav_items"><a href="#testimonials">Projects</a></li>
+                        <li class="nav_items"><a href="#reachout">Contact Us</a></li>
+                        </ul>
+                    <!-- </nav> -->
+                </div>
+                
+                <!-- <div> -->
+                    <div class="massive_text_container" style="float:right;width:48.4vw; height:107vh;margin-top:10%;">
+                        <img style="max-width:100%;" class="pure-img" src="./img_laptop.png">
+                    </div>
+                    <div class="massive_text_container">
+                        <div class="massive_text">
+                            <p>INGENIOUS DESIGN & DEVELOPMENT</p>
+                        </div>
+                    </div>
+                <!-- </div> -->
+                
+                <!-- <div style="float:left;">
+                        <button style="" class=""><a href="">About Us</a></button>
+                </div> -->
+                
+            </div>
+            <!-- </div> -->
+            <!-- /Header -->
+
+            <!-- About US -->
+            <div class="aboutus">
+                a
+            </div>
+            <!-- About US -->
+
+            <!--Services -->
+            <div class="services">
+                b
+            </div>
+            <!--/Services -->
+
+            <!--projects -->
+            <div class="projects">
+                c
+            </div>
+            <!--/projects -->
+
+            <!--Reviews -->
+            <div class="reviews">
+                d
+            </div>
+            <!--/Reviews -->
+
+            <!--Contact us -->
+            <div class="contactus">
+                e
+            </div>
+            <!--/Contact us -->
+
+            <!--Footer -->
+            <div class="footer">
+                fs
+            </div>
+            <!--/Footer -->
+        </div>
